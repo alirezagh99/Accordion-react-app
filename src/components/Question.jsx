584800b1d622f938f199c8ecef1692e8826038e0
Alireza Ghasemi <agh102724@gmail.com>
2024-12-27 01:43:45 +0330

    add-data

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-function Question() {
+function Question({ id, title, info }) {
   return (
-    <div className="my-7 shadow-xl py-2 border flex flex-row px-5 items-center justify-between w-full">
-      <p className="">Do I Have to allow the use of cookies?</p>
-      <button className=" rounded-full bg-green-400 px-3 py-1 text-lg font-bold shadow text-white">
-        +
-      </button>
+    <div className="my-7 shadow-xl py-2 border px-5  w-full">
+      <div className=" flex flex-row items-center justify-between">
+        <p className="">{title}</p>
+        <button className=" rounded-full bg-green-400 px-3 py-1 text-lg font-bold shadow text-white">
+          +
+        </button>
+      </div>
+      <p className="text-[rgba(0,0,0,0.7)] text-sm">{info}</p>
     </div>
   );
 }
