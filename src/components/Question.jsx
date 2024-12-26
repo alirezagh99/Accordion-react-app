39948687a20eb2a0c402ebd8a4accc1ae22f6f14
Alireza Ghasemi <agh102724@gmail.com>
2024-12-27 01:49:50 +0330

    add-handleClick

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Question({ id, title, info }) {
+  const [showMore, setShowMore] = useState(false);
+  const handleClick = () => {
+    setShowMore(!showMore);
+  };
   return (
-    <div className="my-7 shadow-xl py-2 border px-5  w-full">
+    <div className="my-7 shadow-xl py-2 border px-5 w-full">
       <div className=" flex flex-row items-center justify-between">
         <p className="">{title}</p>
-        <button className=" rounded-full bg-green-400 px-3 py-1 text-lg font-bold shadow text-white">
-          +
+        <button
+          onClick={handleClick}
+          className="rounded-full bg-green-400 px-3 py-1 text-lg font-bold shadow text-white"
+        >
+          {showMore && "-"}
+          {!showMore && "+"}
         </button>
       </div>
-      <p className="text-[rgba(0,0,0,0.7)] text-sm">{info}</p>
+      <p className="text-[rgba(0,0,0,0.7)] text-sm">
+        {showMore && info.substring(0)}
+        {!showMore && info.substring(0, 0)}
+      </p>
     </div>
   );
 }
